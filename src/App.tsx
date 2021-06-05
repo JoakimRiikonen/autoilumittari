e3bed1d4ddcc7d1d928f5a94c951b264ba53cdb6
Joakim Riikonen <>
2021-06-05 12:51:09 +0300

    simple form styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css'
 import { TravelComparisonForm, TravelComparisonFormData } from './containers/TravelComparisonForm'
 import { TravelResults, TravelResult } from './interfaces/TravelResults';
 
--- src/containers/TravelComparisonForm.tsx
+++ src/containers/TravelComparisonForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import './TravelComparisonForm.css'
 
 interface TravelComparisonFormProps {
   onSubmit: (data: TravelComparisonFormData) => void;
@@ -16,22 +17,31 @@ export const TravelComparisonForm = ({ onSubmit }: TravelComparisonFormProps) =>
 
   return(
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor='car'>Auto</label>
-      <select id='car' {...register('car')}>
-        <option value='carA'>Auto A (3.0l / 100km)</option>
-        <option value='carB'>Auto B (3.5l / 100km)</option>
-        <option value='carC'>Auto C (4.0l / 100km)</option>
-      </select>
+      <div className='full'>
+        <label htmlFor='car'>Auto</label>
+        <select id='car' {...register('car')}>
+          <option value='carA'>Auto A (3.0l / 100km)</option>
+          <option value='carB'>Auto B (3.5l / 100km)</option>
+          <option value='carC'>Auto C (4.0l / 100km)</option>
+        </select>
+      </div>
 
-      <label htmlFor='distance'>Matka (km)</label>
-      <input id='distance' {...register('distance')} />
+      <div className='full'>
+        <label htmlFor='distance'>Matka (km)</label>
+        <input id='distance' {...register('distance')} />
+      </div>
 
-      <label htmlFor='speed1'>Nopeus 1 (km/h)</label>
-      <input id='speed1' {...register('speed1')} />
-      <label htmlFor='speed2'>Nopeus 2 (km/h)</label>
-      <input id='speed2' {...register('speed2')} />
-
-      <input type='submit' value='Laske'/>
+        <div className='half'>
+          <label htmlFor='speed1'>Nopeus 1 (km/h)</label>
+          <input id='speed1' {...register('speed1')} />
+        </div>
+        <div className='half'>
+          <label htmlFor='speed2'>Nopeus 2 (km/h)</label>
+          <input id='speed2' {...register('speed2')} />
+        </div>
+      <div className='full'>
+        <input type='submit' value='Laske'/>
+      </div>
     </form>
   )
   
